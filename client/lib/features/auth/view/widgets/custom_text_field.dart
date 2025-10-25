d1a8d4fbce521704c10f111a83efcd09277d9d2c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-10-25 19:36:56 -0400

    added a form to the signup page.

--- client/lib/features/auth/view/widgets/custom_text_field.dart
+++ client/lib/features/auth/view/widgets/custom_text_field.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final String hintText;
-  const CustomTextField({super.key, required this.hintText});
+  final TextEditingController controller;
+  final bool isTextObscured;
+
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isTextObscured = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(decoration: InputDecoration(hintText: hintText));
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(hintText: hintText),
+      obscureText: isTextObscured,
+      validator: (value) {
+        if (value!.trim().isEmpty) {
+          return "$hintText is missing!";
+        }
+        return null;
+      },
+    );
   }
 }
